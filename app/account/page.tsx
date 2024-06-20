4e5336004d4f8e5f349a0a086ad1686991b7bf14
Egor Esensky <egortus@gmail.com>
2024-06-20 23:00:28 +0300

    main finished

--- app/account/page.tsx
+++ app/account/page.tsx
@@ -6,21 +6,32 @@ import styles from "./page.module.css";
 
 export default function Account() {
 
-  const Cell = ({header, bigText, info}: {header?: string, bigText?: string, info?: string}) => (
+  const Cell = ({ header, bigText, info, img }: { header?: string, bigText?: string, info?: string, img?: string }) => (
     <div className={styles.cell}>
       <h6>{header}</h6>
-      <h1>{bigText}</h1>
-      <p>{info}</p>
+      <span className={styles.count}>{bigText}</span>
+      <p className={styles.numberPlus}>{info}</p>
+    </div>
+  );
+
+  const CellImg = ({ header, bigText, info, img }: { header?: string, bigText?: string, info?: string, img?: string }) => (
+    <div className={styles.cellImg}>
+      <h6>{header}</h6>
+      <span className={styles.count}>{bigText}</span>
+      <p className={styles.numberPlus}>{info}</p>
+      <img src="/historyLook.png" />
     </div>
   );
 
   //большая
-  const CellBig = ({header, info}: {header: string, info: string}) => (
-    <div className={`${styles.cell} ${styles.large}`}>
-      <h6>{header}</h6>
-      <p>{info}</p>
-    </div>
-  );
+  // const CellBig = ({header, info, weather}: {header: string, info: string, weather: string}) => (
+  //   <div className={`${styles.cell} ${styles.large}`}>
+  //     <h6>{header}</h6>
+  //     <p>{info}</p>
+  //     <p>{weather}</p>
+  //   </div>
+  // );
+
 
 
   return (
@@ -35,17 +46,50 @@ export default function Account() {
 
       <div className={styles.grid}>
         <div className={styles.column}>
-            <Cell header="Заголовок 1" bigText="200" info="Содержимое ячейки 1, объединенной"/>
-            <Cell header="Заголовок 2" bigText="1.2M" info="Содержимое ячейки 2, объединенной"/>
+          <Cell header="Одежда" bigText="200" info="+10 за последний месяц" />
+          <Cell header="Просмотры" bigText="1.2M" info="+10 за последний месяц" />
         </div>
         <div className={styles.column}>
-            <Cell header="Заголовок 3" bigText="15" info="Содержимое ячейки 3, объединенной"/>
-            <Cell header="Заголовок 4" info="Содержимое ячейки 4, объединенной"/>
+          <Cell header="Образы" bigText="15" info="+10 за последний месяц" />
+          <CellImg header="История образов"  />
         </div>
         <div className={`${styles.column} ${styles.large}`}>
-            <CellBig header="Заголовок 4" info="Содержимое ячейки 4, объединенной"/>
+          <div className={`${styles.cell} ${styles.large}`}>
+            <h6>Погода</h6>
+            <div className={styles.gradus}>
+              <img src="/weather.png" className={styles.weatherIcon} />
+              <p className={styles.temperature}>+15º</p>
+            </div>
+            <p style={{margin:'10px 0'}}>📍 Санкт-Петербург</p>
+            <div className={styles.table}>
+              <table>
+                <tbody>
+                  <tr>
+                    <td>Ср</td>
+                    <td>+16º</td>
+                  </tr>
+                  <tr>
+                    <td>Чт</td>
+                    <td>+17º</td>
+                  </tr>
+                  <tr>
+                    <td>Пт</td>
+                    <td>+14º</td>
+                  </tr>
+                  <tr>
+                    <td>Сб</td>
+                    <td>+19º</td>
+                  </tr>
+                  <tr>
+                    <td>Вс</td>
+                    <td>+21º</td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
         </div>
-    </div>
+      </div>
+    </div >
     </>
   );
 }
