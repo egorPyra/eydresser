ad1504ababfb58b38adf29439f798507d50f1729
Egor Esensky <egortus@gmail.com>
2024-10-03 18:51:42 +0300

    placeholder

--- app/api/outfits/route.ts
+++ app/api/outfits/route.ts
@@ -1,36 +1,9 @@
-// pages/api/outfits/index.ts
-import { NextApiRequest, NextApiResponse } from 'next';
-import { prisma } from '../../../prisma/prisma-client'; // Подключение Prisma
+import { NextResponse } from 'next/server';
 
-export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-  if (req.method === 'POST') {
-    const { name, userId, clothes } = req.body;
-
-    try {
-      const outfit = await prisma.outfit.create({
-        data: {
-          name,
-          userId,
-          clothes: {
-            create: clothes.map((item: { src: string; alt: string; position: { x: number; y: number }; rotationAngle: number }) => ({
-              clothes: {
-                create: {
-                  name: item.alt,
-                  imageUrl: item.src,
-                },
-              },
-            })),
-          },
-        },
-      });
-
-      res.status(200).json(outfit);
-    } catch (error) {
-      console.error("Failed to save outfit:", error);
-      res.status(500).json({ error: 'Failed to save outfit' });
-    }
-  } else {
-    res.status(405).json({ message: 'Method not allowed' });
-  }
+export async function GET() {
+  return NextResponse.json({ message: "This is a placeholder for the GET request" }, { status: 200 });
 }
 
+export async function POST(request: Request) {
+  return NextResponse.json({ message: "This is a placeholder for the POST request" }, { status: 200 });
+}
